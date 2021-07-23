66d4d0985dc24efe3e9d3fee4a87a339da89a8bd
tbor00 <btimoteo@outlook.com>
2021-07-23 00:20:48 -0300

    First Commit React And vite, is Awesome

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <div className="container-title">
+        <h2><span className="c-blue">Learn</span> React And <span className="c-yellow">Vite</span> in an awesome way</h2>
+       <a href="https://vitejs.dev/" target="__blank"> <img src="https://vitejs.dev/logo.svg" alt="Vite and React" height="300"  className="App-vite"/></a>
+      </div>
+    </>
+  )
+}
+
+export default App
